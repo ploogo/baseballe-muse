42f1b35df79347a6c9dbdb58d06c08a563e41f52
Jimmy Romanowski <jro.creative@gmail.com>
2022-12-19 08:19:02 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `baseball muse`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
