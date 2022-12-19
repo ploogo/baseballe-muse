ff4f1a67213780f1c221d415128284b2f0053725
Jimmy Romanowski <jro.creative@gmail.com>
2022-12-18 22:50:01 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `baseball muse`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
